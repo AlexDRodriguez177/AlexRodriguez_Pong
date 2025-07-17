b3af0271527679b8f786f53e478aa3690d10ff55
Alex Rodriguez <alex.rodriguez6@g.austincc.edu>
2025-07-17 15:01:01 -0500

    Got rid of the magic number of 1f in Random.Range and used a new variable ballDirectionRange to store the new variable.
    added summaries and comments
    
    organised the variables

--- Assets/Scripting/Ball.cs
+++ Assets/Scripting/Ball.cs
@@ -3,39 +3,61 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    //Ball vaiables
     public enum CollisionTag
     {
         BounceWall,
         Player,
         ScoreWall,
     }
-    
     [SerializeField] private float ballSpeed = 8f;
     [SerializeField] private List<string> collisionTags;
-    
     private Vector2 ballDirection;
-
+    private float ballDirectionRange = 1f;
+    // Audio variables
     [SerializeField] public AudioSource ballAudioScource;
     [SerializeField] public AudioClip wallBounceAudio;
     [SerializeField] public AudioClip playerBounceAudio;
     [SerializeField] public AudioClip scoreAudio;
+
+
+    /// <summary>
+    /// Initializes the ball's position
+    /// Pushes the ball in a random direction when spawned
+    /// </summary>
     void Start()
     {
         transform.position = Vector2.zero;
-        ballDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        ballDirection = new Vector2(Random.Range(-ballDirectionRange, ballDirectionRange), Random.Range(-ballDirectionRange, ballDirectionRange)).normalized;
     }
-
+    /// <summary>
+    /// Grabs the AudioSource component from the ball
+    /// Moves the ball in the direction it is facing
+    /// Uses ball speed to determine how fast it moves
+    /// Uses Time.deltaTime to make the speed consistent across different frame rates
+    /// </summary>
     void Update()
     {
         ballAudioScource= GetComponent<AudioSource>();
         transform.Translate(ballDirection * ballSpeed * Time.deltaTime);
     }
-
+    /// <summary>
+    /// resets the ball's position to the center of the screen (0,0)
+    /// Pushes the ball in a random direction
+    /// </summary>
     private void ResetBall()
     {
         transform.position = Vector2.zero;
-        ballDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        ballDirection = new Vector2(Random.Range(-ballDirectionRange, ballDirectionRange), Random.Range(-ballDirectionRange, ballDirectionRange)).normalized;
     }
+    /// <summary>
+    /// Chechs if the ball collides with any of the walls, player, or goal
+    /// Plays the appropriate audio clip based on the collision
+    /// If goal is scored, resets the ball and increments the score for the player who scored
+    /// If the ball collides with a wall it bounces off in the opposite direction
+    /// If the ball collides with a player it will bounce off in the opposite direction it will also adjust the y direction based on the player's position
+    /// The bal's direction is normalized to ensure consistent speed
+    /// </summary>
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag(collisionTags[(int)CollisionTag.ScoreWall]))
