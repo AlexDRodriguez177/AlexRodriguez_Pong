4252c5e978f30692f4143c77a76f914359ff0254
Alex Rodriguez <alex.rodriguez6@g.austincc.edu>
2025-07-17 14:39:01 -0500

    Added summary comments into the GameManager Script I also added a few comments

--- Assets/Scripting/GameManager.cs
+++ Assets/Scripting/GameManager.cs
@@ -3,10 +3,13 @@ using TMPro;
 using System.Collections.Generic;
 public class GameManager : MonoBehaviour
 {
+    //Score variables 
     [SerializeField] private List<int> playerScores;
     [SerializeField] private List<TextMeshProUGUI> playerScoreTexts;
-
     public static GameManager instance;
+    /// <summary>
+    /// Initalizes some logic to make sure only one GameManager exists in the scene.
+    /// </summary>
     void Start()
     {
         // destroys any existing GameManager instance in the scene
@@ -17,7 +20,12 @@ public class GameManager : MonoBehaviour
         }
         instance = this;
     }
-
+    /// <summary>
+    /// Changes the players score
+    /// Stores it was an integer and adds 1 each goal
+    /// Stores the score integer into a TextMeshProUGUI object
+    /// Converts the integer into a string to display it in the UI
+    /// </summary>
     public static void IncrementScore(PlayerType playerType)
     {
         if (instance == null) return;
